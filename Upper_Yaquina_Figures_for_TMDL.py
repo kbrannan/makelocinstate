08cc2aaa60a3fd0c281694d89fa788e317b03c3f
Creutzx <creutzburg.brian@deq.state.or.us>
2017-12-28 13:05:43 -0800

    first version of the py file

---
+++ Upper_Yaquina_Figures_for_TMDL.py
@@ -0,0 +1,59 @@
+import arcpy
+
+#input info in same way as Mid-Coast map outline
+#created new .lyr and .mxd files specific to upper yaquina watershed
+
+chr_dir_work = r'\\DEQHQ1\TMDL_WR\MidCoast\GIS\Figures\python\makelocinstate'
+chr_map_doc_base = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_BC_Automated_V2.mxd'
+chr_map_doc_new = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\python\makelocinstate\Upper_Yaquina.mxd'
+chr_png_map_new = r'\\DEQHQ1\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Up_Yaq.png'
+chr_lyr_name_sel = r'HUC 10'
+chr_lyr_sym_wtsd = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Layer\upper_yaquina_official_huc_10.lyr'
+chr_lyr_sym_state = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\or_state_boundary_for_inset.lyr'
+
+# get the base map document
+
+mxd_base = arcpy.mapping.MapDocument(chr_map_doc_base)
+# make a copy that will be changed
+
+mxd_base.saveACopy(chr_map_doc_new)
+del mxd_base
+# get new map doc to edit
+mxd_new = arcpy.mapping.MapDocument(chr_map_doc_new)
+layers = arcpy.mapping.ListLayers(mxd_new)
+print '\n' + r'Data sources of layers'
+for layer in layers:
+    if layer.supports("dataSource"):
+        print layer.dataSource
+# some layers might not support the property "dataSource"
+print '\n' + r'Names of layers'
+#getting an error on line 32: what is the purpose of using the Describe function here?
+for layer in layers:
+    desc = arcpy.Describe(r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\or_state_boundary_for_inset.lyr')
+    print desc.nameString
+
+# get the data frame of the map doc
+df = arcpy.mapping.ListDataFrames(mxd_new)[0]
+# get the watershed layer in map doc
+lyr_wtsd = arcpy.mapping.ListLayers(mxd_new, chr_lyr_name_sel, df)[0]
+# get the watershed boundary and state layers that have the symbology to be used
+    #lyr_sym_wtsd = arcpy.mapping.Layer(chr_lyr_sym_wtsd)
+    #lyr_sym_state = arcpy.mapping.Layer(chr_lyr_sym_state)
+# change the symbology of the watershed boundary in map doc to that of the symbology of the layer file
+    #arcpy.mapping.UpdateLayer(df,lyr_wtsd, lyr_sym_wtsd)
+# reset the map extent to that of the state layer file
+    #df.extent = lyr_sym_state.getExtent()
+# save map doc
+    #mxd_new.save()
+# export map to PNG file
+    #arcpy.mapping.ExportToPNG(mxd_new,chr_png_map_new, resolution=300)
+
+
+
+
+
+
+#mxd_base = arcpy.mapping.MapDocument(r'\\U:\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_BC_Automated_V2.mxd')
+
+
+
