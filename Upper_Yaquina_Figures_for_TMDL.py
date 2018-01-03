cb104c4ef0b722ad1f30465e25b19648ba154cfa
Creutzx <creutzburg.brian@deq.state.or.us>
2018-01-03 15:27:57 -0800

    With KB Edits

--- Upper_Yaquina_Figures_for_TMDL.py
+++ Upper_Yaquina_Figures_for_TMDL.py
@@ -5,7 +5,7 @@ import arcpy
 
 chr_dir_work = r'\\DEQHQ1\TMDL_WR\MidCoast\GIS\Figures\python\makelocinstate'
 chr_map_doc_base = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_BC_Automated_V2.mxd'
-chr_map_doc_new = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\python\makelocinstate\Upper_Yaquina.mxd'
+chr_map_doc_new = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\python\makelocinstate\Upper_Yaquina_KMB.mxd'
 chr_png_map_new = r'\\DEQHQ1\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Up_Yaq.png'
 chr_lyr_name_sel = r'HUC 10'
 chr_lyr_sym_wtsd = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Layer\upper_yaquina_official_huc_10.lyr'
@@ -28,6 +28,15 @@ for layer in layers:
 # some layers might not support the property "dataSource"
 print '\n' + r'Names of layers'
 #getting an error on line 32: what is the purpose of using the Describe function here?
+# use the following command to get the layer file and create a layer object in your code
+layerCur = arcpy.mapping.Layer(r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\or_state_boundary_for_inset.lyr')
+# then you can get it's properties and maniputale it
+print layerCur.name
+# the describe command only works on objects that are created within arcpy, but you don't have to use it with the layer object
+#desc = arcpy.Describe(r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\or_state_boundary_for_inset.lyr')
+#print desc.nameString
+#print desc
+
 for layer in layers:
     desc = arcpy.Describe(r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\or_state_boundary_for_inset.lyr')
     print desc.nameString
@@ -54,6 +63,4 @@ lyr_wtsd = arcpy.mapping.ListLayers(mxd_new, chr_lyr_name_sel, df)[0]
 
 
 #mxd_base = arcpy.mapping.MapDocument(r'\\U:\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_BC_Automated_V2.mxd')
-
-
 
